506ac35071454ffefb4918183701f6bde4785f18
galvysRodriguez <grodriguez.7222@unimar.edu.ve>
2024-11-25 02:00:18 -0400

    ...

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -40,8 +40,7 @@ export default function Home() {
             body: new URLSearchParams({
                 username: formData.correo, // Usar los datos del formulario
                 password: formData.contraseña, // Usar los datos del formulario
-            }),
-            credentials: 'include'
+            })
         })
         .then(res => {
             if (!res.ok) {
