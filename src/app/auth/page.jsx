09abb70f16ffce9609cdb07c4e482200341af395
galvysRodriguez <grodriguez.7222@unimar.edu.ve>
2024-11-25 02:03:56 -0400

    iniciar sesion y grafica

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -36,11 +36,14 @@ export default function Home() {
         e.preventDefault();
         fetch(`${API_URL}/auth/login`, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: new URLSearchParams({
-                username: formData.correo, // Usar los datos del formulario
-                password: formData.contraseña, // Usar los datos del formulario
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: formData.correo,
+                password: formData.contraseña,
             })
+        
         })
         .then(res => {
             if (!res.ok) {
